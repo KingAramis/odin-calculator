010adc67d7a4b9b22f9367a8122829331984be76
KingAramis <entei.mail@protonmail.com>
2023-03-31 10:02:46 -0700

    Add functions for arthimetic

--- javascript.js
+++ javascript.js
@@ -6,19 +6,24 @@ const displaySmall = document.querySelector(".display-small");
 const numBtns = document.querySelectorAll(".num-btn");
 const operatorBtns = document.querySelectorAll(".operator-btn");
 const equalBtn = document.querySelector(".btn-equal");
+const delBtn = document.querySelector(".del-btn")
+const clearBtn = document.querySelector(".clear-btn");
 
 
 // Add click events for buttons
 
 numBtns.forEach(button => {
-	button.addEventListener('click', (event) => displayMain.textContent 
+	button.addEventListener('click', (event) => displaySmall.textContent 
 			+= button.textContent);
 });
 
 operatorBtns.forEach(button => {
     button.addEventListener('click', (event) => {
-    const lastCharBool = handleOperator();
-        if (lastCharBool) displayMain.textContent += button.textContent;
+    let lastCharNum = handleOperator();
+    const hasOperator = getOperator();
+    if (hasOperator !== null && lastCharNum === true) calculate();
+    lastCharNum = handleOperator();
+    if (lastCharNum) displaySmall.textContent += button.textContent;
     });
 });
 
@@ -29,12 +34,16 @@ equalBtn.addEventListener('click', (event) => {
     calculate();
 });
 
+delBtn.addEventListener('click', (event) => deleteChar());
+
+clearBtn.addEventListener('click', (event) => clearDisplay());
+
 
 // functions to handle exceptions
 
 function handleOperator() {
-	const lastChar = displayMain.textContent[
-			displayMain.textContent.length -1];
+	const lastChar = displaySmall.textContent[
+			displaySmall.textContent.length -1];
     if (lastChar === "+" || lastChar === "-" || lastChar === "*" ||
         lastChar === "/" || lastChar === "%" || lastChar === ".") {
             return false;
@@ -46,7 +55,7 @@ function handleOperator() {
 // functions for calculate function
 
 function calculate() {
-    const operator = checkForOperator();
+    const operator = getOperator();
     const intArray = getIntArray(operator);
     switch (operator) {
         case "+":
@@ -65,10 +74,10 @@ function calculate() {
             percentageNumbers(intArray);
             break;
     };
-}
+};
 
-function checkForOperator() {
-    let equation = displayMain.textContent;
+function getOperator() {
+    let equation = displaySmall.textContent;
     for (let i = 0; i < equation.length - 1; i++) {
 		if (equation[i] === '+' || equation[i] === '-' || equation[i] === '*'
 			|| equation[i] === '/' || equation[i] === "%") return equation[i];
@@ -77,7 +86,7 @@ function checkForOperator() {
 };
 
 function getIntArray(operator) {
-    const equArray = displayMain.textContent.split(operator);
+    const equArray = displaySmall.textContent.split(operator);
     const intArray = equArray.map((numberString) => {
         return Number(numberString)
     });
@@ -87,20 +96,37 @@ function getIntArray(operator) {
 // Arithmetic operation functions
 
 function addNumbers(intArray) {
-    console.log(intArray[0] + intArray[1]);
+    displayMain.textContent = intArray[0] + intArray[1];
+    displaySmall.textContent = intArray[0] + intArray[1];
 };
 
 function subtractNumbers(intArray) {
-	console.log(intArray[0] - intArray[1];
+    displayMain.textContent = intArray[0] - intArray[1];
+    displaySmall.textContent = intArray[0] - intArray[1];
 };
     
-function mulyiplyNumbers(intArray) {
-	console.log(intArray[0] * intArray[1];
+function multiplyNumbers(intArray) {
+    displayMain.textContent = intArray[0] * intArray[1];
+    displaySmall.textContent = intArray[0] * intArray[1];
 };
 
 function divideNumbers(intArray) {
-	console.log(intArray[0] / intArray[1];
+    displayMain.textContent = intArray[0] / intArray[1];
+    displaySmall.textContent = intArray[0] / intArray[1];
 };
+
+// Clear and delete
+
+function clearDisplay() {
+    displaySmall.textContent = "";
+    displayMain.textContent = "";
+};
+
+function deleteChar() {
+    const equationString = displaySmall.textContent;
+    displaySmall.textContent = equationString.slice(0, -1);
+};
+
 
 
 // to do 
